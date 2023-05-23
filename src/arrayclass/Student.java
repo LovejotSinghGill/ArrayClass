d612d3f173a2747a775b021f56c937949acd5a2f
LovejotSinghGill <133809229+LovejotSinghGill@users.noreply.github.com>
2023-05-23 13:53:29 -0400

    Update Student.java

--- src/arrayclass/Student.java
+++ src/arrayclass/Student.java
@@ -6,6 +6,7 @@ public class Student {
     //added first commment
     // added comment for fetch and merge
     // one more comment
+    // second comment for pull
     private String studentId;
     private String studentName;
 
